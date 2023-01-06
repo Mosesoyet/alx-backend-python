d916572d11449e02a8fd62e65c79785f152bd9b0
= <=>
2023-01-07 00:22:03 +0200

    Add union form typing

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -2,11 +2,11 @@
 """
 A program to sum mix list elements as float
 """
-from typing import List
+from typing import List Union
 
 
-def sum_mixed_list(mxd_lst: List[int, float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
     return float sum of any list element
     """
-    return float(sum(mxd_lst))
+    return sum(mxd_lst)
