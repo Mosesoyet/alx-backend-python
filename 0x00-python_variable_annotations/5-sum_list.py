71a0b761c4f168dcc6ea2bad61198bfb69461ba3
= <=>
2023-01-06 23:57:23 +0200

    Modify

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -4,8 +4,8 @@ a function that returns sum of floats
 """
 
 
-def sum_list(input_list: list[float]) -> list[float]:
+def sum_list(input_list: list[float]) -> float:
     """
     return sum of list elemets
     """
-    return sum(input_list)
+    return sum_list += i for i in input_list
