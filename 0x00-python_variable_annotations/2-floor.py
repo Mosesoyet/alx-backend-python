6dfd5535ff09bcd971a02b765116b1194a20052d
= <=>
2023-01-06 22:44:07 +0200

    Add floor function to round off the number to the nearest value

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+A function that returns floor of n
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    rounds off number to the nearest value
+    """
+    return math.floor(n)
