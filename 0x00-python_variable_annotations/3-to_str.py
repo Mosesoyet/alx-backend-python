3339636a948f31a8820fceb598b202567ed2752c
= <=>
2023-01-06 22:50:09 +0200

    Use str() to convert n

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+a function that takes a float and return it's string representation
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    return string of n
+    """
+    return str(n)
