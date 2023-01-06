59cde4e11c960a155c7ea6328e0e7e4af5fb804d
= <=>
2023-01-06 23:45:31 +0200

    Remove annotation on list

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,10 +2,9 @@
 """
 a function that returns sum of floats
 """
-import math
 
 
-def sum_list(input_list: float) -> float:
+def sum_list(input_list) -> float:
     """
     return sum of list elemets
     """
