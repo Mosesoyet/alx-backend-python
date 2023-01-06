59b358b4c9baec4841a04168c861bf1f15b0ce5e
= <=>
2023-01-06 22:38:40 +0200

    Concat two strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+A function that concatinates two strings
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    return concatination of the two strings
+    """
+    return str1 + str2
