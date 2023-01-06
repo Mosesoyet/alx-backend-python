1a83543cfe069ad4b1b49aabfc17097450679c75
= <=>
2023-01-06 23:20:36 +0200

    Use math.sum() to add list elements

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+a function that returns sum of floats
+"""
+import math
+
+
+def sum_list(input_list: float) -> float:
+    """
+    return sum of the list elements
+    """
+    return sum(input_list)
