459351fc6ed72e868070c91fc6df9770735ff774
= <=>
2023-01-09 19:32:46 +0200

    Fix code

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Task 2 modules
+"""
+import asyncio
+import time
+
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """
+    computes the average runtime
+    """
+    start = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    return (time.time() - start) / n
