cb463e14acda6c54ff21896648090b16a25a284d
= <=>
2023-01-31 01:00:19 +0200

    Test for access_map

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+Utils's test module
+"""
+import unittest
+from typing import Dict, Tuple, Union
+from unittest.mock import patch, Mock
+from parameterized import parameterized
+
+from utils import (
+        access_nested_map,
+        get_json,
+        memoize,
+)
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """Test access_nested_map function
+    """
+    @parameterized.expand([
+    ({'a': 1}, ('a',), 1),
+    ({'a': {'b': 2}}, ('a',), {'b': 2}),
+    ({'a': {'b': 2}}, ('a', 'b'), 2),
+    ])
+    
+
+    def test_access_nested_map(
+            self,
+            nested_map: Dict,
+            path: Tuple[str],
+            expected: Union[Dict, int],
+            ) -> None:
+        """Test access_nested map's output
+        """
+        self.assertEqual(access_nested_map(nested_map, path), expected)
